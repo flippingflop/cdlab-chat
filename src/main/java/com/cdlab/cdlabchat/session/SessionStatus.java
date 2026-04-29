add1c6fbd40d74c3d43dfd7eb570906a1c7b568a
KIM DONGHYEON <79964892+flippingflop@users.noreply.github.com>
2026-04-29 20:01:20 +0900

    docs: 세션 상태 enum에 설명 주석 추가

--- src/main/java/com/cdlab/cdlabchat/session/SessionStatus.java
+++ src/main/java/com/cdlab/cdlabchat/session/SessionStatus.java
@@ -1,7 +1,7 @@
 package com.cdlab.cdlabchat.session;
 
 public enum SessionStatus {
-    ACTIVE,
-    SUSPENDED,
-    ENDED
+    ACTIVE, // 두 사람 모두가 연결된 상태
+    SUSPENDED, // 둘 중 한 사람 이상의 웹소켓 연결 불안정으로 인해 일시적으로 연결이 끊어진 상태
+    ENDED // 둘 중 한 사람 이상이 명시적으로 세션에서 퇴장한 상태
 }
